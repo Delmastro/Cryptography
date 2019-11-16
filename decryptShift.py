c64cdacc8201de1f83844e9caf4a158b0360e784
Michael Delmastro <42367211+Delmastro@users.noreply.github.com>
2019-11-16 11:32:03 +1000

    Update decryptShift.py

--- decryptShift.py
+++ decryptShift.py
@@ -1,1 +1,16 @@
 
+def decrypt_shift_cipher():
+
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    ciphertext = input("Enter the encrpyted text: ").lower()
+    decrypted_list = []
+
+    for shift_key in range(0,26):
+        plaintext = ""
+        for letter in ciphertext:
+            plaintext += alpha[(alpha.index(letter) + shift_key) % len(alpha)]
+        decrypted_list.append(plaintext)
+    [print(i, plain.upper()) for i, plain in enumerate(decrypted_list)]
+    
+decrypt_shift_cipher()
+
